cc590a95e194c4903a70f60093130e413d664c27
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-04-02 21:24:25 -0400

    Made some progress in expanding fake promise implementation

--- sandbox/fake-promise.js
+++ sandbox/fake-promise.js
@@ -1,104 +1,134 @@
 class FakePromise {
   constructor(executor) {
-    this.privResolve = function privResolve(value) {
-      if (this.isResolved) {
-        return;
-      }
+    this.private = {
+      state: 'pending',
+      resolve: (value) => {
+        if (this.private.state === 'pending') {
+          this.private.state = 'resolvedUnsent';
+          this.private.value = value;
+        }
 
-      this.privValue = value;
-      this.isResolved = true;
+        if (this.private.state === 'resolvedUnsent' && this.private.handleResolve) {
+          this.private.state = 'resolvedSent';
+          queueMicrotask(() => {
+            try {
+              const result = this.private.handleResolve(this.private.value);
 
-      if (this.resolve) {
-        console.log('fakePromise resolved in constructor');
-        queueMicrotask(() => this.resolve(value));
-      }
+              if (!this.private.nextPromise) {
+                this.private.nextPromise = new FakePromise((resolve) => resolve(result));
+              }
+            } catch (err) {
+              this.private.nextPromise = new FakePromise((resolve, reject) => reject(err));
+            }
+          });
+        }
+      },
+      reject: (reason) => {
+        if (this.private.state === 'pending') {
+          this.private.state = 'rejectedUnsent';
+          this.private.reason = reason;
+        }
+
+        if (this.private.state === 'rejectedUnsent' && this.private.handleReject) {
+          this.private.state = 'rejectedSent';
+          queueMicrotask(() => {
+            try {
+              const result = this.private.handleReject(this.private.reason);
+
+              this.private.nextPromise = new FakePromise((resolve) => resolve(result));
+            } catch (err) {
+              this.private.nextPromise = new FakePromise((resolve, reject) => reject(err));
+            }
+          });
+        }
+      },
     };
 
-    this.privReject = function privReject(reason) {
-      if (this.isRejected) {
-        return;
-      }
-
-      this.privReason = reason;
-      this.isRejected = true;
-
-      if (this.reject) {
-        console.log('fakePromise rejected in constructor');
-        queueMicrotask(() => this.reject(reason));
-      }
-    };
-
-    executor(this.privResolve.bind(this), this.privReject.bind(this));
+    executor(this.private.resolve, this.private.reject);
   }
 
+  // Does not implement calling then more than once on the same promise. Only a handler from the
+  // last then call before the promise is resolved or rejected will be called.
   then(resolve, reject) {
-    this.resolve = resolve;
-    this.reject = reject;
+    this.private.handleResolve = resolve;
+    this.private.handleReject = reject;
+    this.private.resolve();
+    this.private.reject();
 
-    if (this.isResolved) {
-      console.log('fakePromise resolved in then');
-      resolve(this.privValue);
-      return null;
-    }
+    this.private.nextPromise = this.private.nextPromise
+      || new FakePromise((nextResolve, nextReject) => {
+        nextResolve(this.private.result);
+      });
 
-    if (this.isRejected) {
-      console.log('fakePromise rejected in then');
-      reject(this.privReason);
-      return null;
-    }
+    return this.private.nextPromise;
+  }
 
-    return null;
+  catch(reject) {
+    return this.then(null, reject);
   }
 }
 
-const realPromise1 = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log('realPromise1 setTimeout callback ran a');
-    resolve('Success!');
-    resolve('Success again!');
-    console.log('realPromise1 setTimeout callback ran b');
-  }, 0);
+// const realPromise1 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log('realPromise1 setTimeout callback ran a');
+//     resolve('Success!');
+//     resolve('Success again!');
+//     console.log('realPromise1 setTimeout callback ran b');
+//   }, 0);
+// });
+
+// realPromise1.then((successMessage) => {
+//   console.log(`realPromise1 Yay! ${successMessage}`);
+// });
+
+// const realPromise2 = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log('realPromise2 setTimeout callback ran a');
+//     resolve('Success!');
+//     resolve('Success again!');
+//     console.log('realPromise2 setTimeout callback ran b');
+//   }, 0);
+// });
+
+// realPromise2.then((successMessage) => {
+//   console.log(`realPromise2 Yay! ${successMessage}`);
+// });
+
+// const fakePromise1 = new FakePromise((resolve) => {
+//   setTimeout(() => {
+//     console.log('fakePromise1 setTimeout callback ran a');
+//     resolve('Success!');
+//     resolve('Success again!');
+//     console.log('fakePromise1 setTimeout callback ran b');
+//   }, 0);
+// });
+
+// fakePromise1.then((successMessage) => {
+//   console.log(`fakePromise1 Yay! ${successMessage}`);
+// });
+
+// const fakePromise2 = new FakePromise((resolve) => {
+//   setTimeout(() => {
+//     console.log('fakePromise2 setTimeout callback ran a');
+//     resolve('Success!');
+//     resolve('Success again!');
+//     console.log('fakePromise2 setTimeout callback ran b');
+//   }, 0);
+// });
+
+// setImmediate(() => fakePromise2.then((successMessage) => {
+//   console.log(`fakePromise2 Yay! ${successMessage}`);
+// }));
+
+const promise = new Promise((resolve) => {
+  console.time('for loop duration');
+  console.log('for loop started');
+  for (let i = 0; i < 1000000000; i += 1);
+  console.log('for loop ended');
+  console.timeEnd('for loop duration');
+  resolve();
 });
 
-realPromise1.then((successMessage) => {
-  console.log(`realPromise1 Yay! ${successMessage}`);
-});
+console.log('promise created');
 
-const realPromise2 = new Promise((resolve) => {
-  setTimeout(() => {
-    console.log('realPromise2 setTimeout callback ran a');
-    resolve('Success!');
-    resolve('Success again!');
-    console.log('realPromise2 setTimeout callback ran b');
-  }, 0);
-});
-
-realPromise2.then((successMessage) => {
-  console.log(`realPromise2 Yay! ${successMessage}`);
-});
-
-const fakePromise1 = new FakePromise((resolve) => {
-  setTimeout(() => {
-    console.log('fakePromise1 setTimeout callback ran a');
-    resolve('Success!');
-    resolve('Success again!');
-    console.log('fakePromise1 setTimeout callback ran b');
-  }, 0);
-});
-
-fakePromise1.then((successMessage) => {
-  console.log(`fakePromise1 Yay! ${successMessage}`);
-});
-
-const fakePromise2 = new FakePromise((resolve) => {
-  setTimeout(() => {
-    console.log('fakePromise2 setTimeout callback ran a');
-    resolve('Success!');
-    resolve('Success again!');
-    console.log('fakePromise2 setTimeout callback ran b');
-  }, 0);
-});
-
-setImmediate(() => fakePromise2.then((successMessage) => {
-  console.log(`fakePromise2 Yay! ${successMessage}`);
-}));
+promise.then(() => console.log('promise resolved'));
