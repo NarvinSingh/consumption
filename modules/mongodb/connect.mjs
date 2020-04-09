9bf5a916080c7eaaaa42a2308f51f5eda4fdbce7
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-04-08 23:17:27 -0400

    Updated connect to fix dbName

--- modules/mongodb/connect.mjs
+++ modules/mongodb/connect.mjs
@@ -1,17 +1,16 @@
 import 'dotenv/config.js';
 import mongodb from 'mongodb';
 
-async function connect(
+export default async function connect(
   host = process.env.MONGODB_HOST,
   dbName = process.env.MONGODB_DB,
   username = process.env.MONGODB_USERNAME,
   password = process.env.MONGODB_PASSWORD,
 ) {
   const { MongoClient } = mongodb;
-  const uri = `mongodb+srv://${username}:${password}@${host}/${dbName}?retryWrites=true&w=majority`;
+  const fixedDbName = dbName === undefined || dbName === null ? '' : dbName;
+  const uri = `mongodb+srv://${username}:${password}@${host}/${fixedDbName}?retryWrites=true&w=majority`;
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
   return client.connect();
 }
-
-export default connect;
