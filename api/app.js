0935b6ca19e75c1fdb802e67d880218d6ad5c02d
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-04-11 23:46:49 -0400

    Implemented connecting to database and cleanup with the Cleanup class

--- api/app.js
+++ api/app.js
@@ -1,15 +1,59 @@
 import 'dotenv/config.js';
 import express from 'express';
+import Cleanup from '../modules/cleanup.mjs';
+import connect from '../modules/mongodb/connect.mjs';
 import registerRouter from './register-router.mjs';
 import loginRouter from './login-router.mjs';
 import logoutRouter from './logout-router.mjs';
 
-const app = express();
+const cleanup = new Cleanup();
 
-app.use('/api/register', registerRouter);
-app.use('/api/login', loginRouter);
-app.use('/api/logout', logoutRouter);
+// Connect to the db first
+console.log(`Connecting to database ${process.env.MONGODB_DB}...`);
+connect().then((client) => {
+  if (!client.isConnected) {
+    throw new Error('Database is disconnected');
+  }
 
-app.listen(process.env.APP_PORT, () => {
-  console.log(`API app is listening on port ${process.env.APP_PORT}`);
+  console.log(`Connected to database ${process.env.MONGODB_DB}`);
+  cleanup.addCallbacks(async () => {
+    if (client.isConnected) {
+      try {
+        await client.close();
+        console.log('Database connection closed');
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+
+  return client;
+
+// Only start the server once connected to the db
+}).then((client) => {
+  const app = express();
+
+  app.use('/api/register', registerRouter);
+  app.use('/api/login', loginRouter);
+  app.use('/api/logout', logoutRouter);
+
+  const server = app.listen(process.env.API_PORT, () => {
+    console.log(`The server is listening on port ${process.env.API_PORT}...`);
+  });
+
+  cleanup.addCallbacks(async () => {
+    if (server.listening) {
+      try {
+        await new Promise((resolve) => {
+          server.close(resolve);
+        });
+        console.log('Server closed');
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+}).catch((err) => {
+  console.log(err);
+  cleanup.run('Exception', 1);
 });
