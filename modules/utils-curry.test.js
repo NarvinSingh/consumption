01a47fc5377cbdaa792ddac9f04a1ed16b245958
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-04-25 22:27:01 -0400

    Updated curry to be a regular function and call the underlying function with a context so it can bound, and updated utils to not use arrow functions

--- modules/utils-curry.test.js
+++ modules/utils-curry.test.js
@@ -4,6 +4,12 @@ describe('Curry tests', () => {
   const square = (x) => x * x;
   const add = (a, b) => a + b;
   const sum = (...ns) => ns.flat().reduce((acc, cur) => acc + cur);
+  const obj = {
+    value: 1,
+    addValue(a) {
+      return a + this.value;
+    },
+  };
 
   test('Test functions work', () => {
     expect(square(4)).toBe(16);
@@ -11,35 +17,36 @@ describe('Curry tests', () => {
     expect(sum(1)).toBe(1);
     expect(sum(1, 2, 3)).toBe(6);
     expect(sum([1, 2, 3])).toBe(6);
+    expect(obj.addValue(2)).toBe(3);
   });
 
   test('Curries a function that has one parameter', () => {
     const square4 = curry(square, 4);
-
     expect(square4()).toBe(16);
   });
 
   test('Curries a function that has two parameters', () => {
     const add2 = curry(add, 2);
-
     expect(add2(3)).toBe(5);
   });
 
   test('Curries a function that has unlimited parameters', () => {
     const sum1 = curry(sum, 1);
-
     expect(sum1(2, 3)).toBe(6);
   });
 
   test('Curries a function twice that has unlimited parameters', () => {
     const sum1and2 = curry(curry(sum, 1), 2);
-
     expect(sum1and2(3)).toBe(6);
   });
 
   test('Curries two arguments to a function that has unlimited parameters', () => {
     const sum1and2 = curry(sum, 1, 2);
-
     expect(sum1and2(3)).toBe(6);
+  });
+
+  test('Curries on object method that can be bound', () => {
+    const addValue = curry(obj.addValue, 2).bind(obj);
+    expect(addValue()).toBe(3);
   });
 });
--- modules/utils.mjs
+++ modules/utils.mjs
@@ -1,10 +1,22 @@
-const curry = (fn, ...params) => (...rest) => fn(...params, ...rest);
-const mix = (...mixins) => mixins.reduceRight((Class, createMixin) => createMixin(Class), Object);
-const promisify = (fn, context = null) => (...rest) => new Promise((resolve, reject) => {
-  fn.call(context, ...rest, (err, value) => {
-    if (err) reject(err);
-    else resolve(value);
-  });
-});
+function curry(fn, ...params) {
+  return function curriedFn(...rest) {
+    return fn.call(this, ...params, ...rest);
+  };
+}
+
+function mix(...mixins) {
+  return mixins.reduceRight((Class, createMixin) => createMixin(Class), Object);
+}
+
+function promisify(fn, context = null) {
+  return function promiseFn(...rest) {
+    return new Promise((resolve, reject) => {
+      fn.call(context, ...rest, (err, value) => {
+        if (err) reject(err);
+        else resolve(value);
+      });
+    });
+  };
+}
 
 export { curry, mix, promisify };
