730eef852ed615f27f06f736e8e21eb2c42573ae
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-04-29 23:12:12 -0400

    Updated ExpressApp to use Models

--- modules/express/express-app.mjs
+++ modules/express/express-app.mjs
@@ -1,62 +1,37 @@
 import express from 'express';
-import connect from '../mongodb/connect.mjs';
 import Cleanup from '../cleanup.mjs';
 import ObservableApp from '../observable-app.mjs';
 import { promisify } from '../utils.mjs';
 
-function connectDbs(app) {
-  const { cleanup } = app;
-
-  return Promise.all(app.dbSpecs.map((spec) => {
-    const { host, db, username, password } = spec;
-
-    const notify = app.makeNotifier(db);
-    const error = app.makeErrorCreator(db);
-
-    notify('connecting');
-    const connectResult = connect(host, db, username, password).then((client) => {
-      if (!client.isConnected()) throw error('failed to connect');
-      notify('connected');
-      return client;
-    }).catch((reason) => { throw error(reason.message, reason); });
-
-    cleanup.addCallbacks(() => connectResult.catch(() => {
-      notify('not connected');
-    }).then((client) => {
-      if (client && client.isConnected()) return client.close();
-      return null;
-    }).then((client) => {
-      if (client === undefined) notify('disconnected');
-    }).catch((reason) => { notify(reason); }));
-
-    return connectResult;
-  }));
-}
-
 const expressApp = (Superclass = Object) => class ExpressApp extends Superclass {
-  constructor(name = 'API', port = 3000, dbSpecs = [], ...superArgs) {
+  constructor(name = 'API', port = 3000, models = [], ...superArgs) {
     super(...superArgs);
     this.name = name;
     this.port = port;
-    this.dbSpecs = dbSpecs;
+    models.forEach((model) => { model.addObservers((data) => this.notifyObservers(data)); });
+    this.models = models;
     this.app = express();
+    this.state = 'stopped';
   }
 
   start() {
-    if (this.server) return this;
+    if (this.state !== 'stopped') return this;
 
+    const { port, app } = this;
     const notify = this.makeNotifier('server');
+
+    this.state = 'starting';
+    notify('starting');
     this.cleanup = new Cleanup();
     this.cleanup.addObservers(notify);
 
-    const { port, app } = this;
-
-    return connectDbs(this).then((clients) => {
-      app.locals.dbClients = clients;
+    return Promise.all(this.models.map((model) => model.connect())).then(() => {
+      if (this.models.some((model) => model.state !== 'connected')) return this.stop();
 
       this.server = app.listen(port);
       const listenResult = promisify(this.server.on, this.server)('listening').then(() => {
         notify('listening', { port });
+        notify('started');
         return this;
       });
 
@@ -66,31 +41,39 @@ const expressApp = (Superclass = Object) => class ExpressApp extends Superclass 
       }).finally(() => this);
 
       this.cleanup.addCallbacks(() => {
-        if (this.server.listening) {
-          return promisify(this.server.close, this.server)().then(() => {
-            notify('closed');
-          }).catch((reason) => {
-            notify(reason);
-          });
+        if (!this.server.listening) {
+          notify('not listening');
+          return Promise.all(this.models.map((model) => model.disconnect()));
         }
-        return notify('not listening');
+
+        notify('closing');
+        return promisify(this.server.close, this.server)().then(() => {
+          notify('closed');
+        }).catch((reason) => {
+          notify(reason);
+        }).finally(() => Promise.all(this.models.map((model) => model.disconnect())));
       });
 
       return Promise.race([listenResult, errorResult]);
     }).catch((reason) => {
-      notify(reason);
+      notify(reason.message, reason);
       return this.stop();
     });
   }
 
   stop() {
-    return this.cleanup
-      ? this.cleanup.runOnce('stop').then(() => {
-        this.server = null;
-        this.cleanup = null;
-        return this;
-      })
-      : Promise.resolve(this);
+    if (this.state === 'stopped') return Promise.resolve(this);
+
+    const notify = this.makeNotifier('server');
+    this.state = 'stopping';
+    notify('stopping');
+    return this.cleanup.runOnce('stop').then(() => {
+      this.server = null;
+      this.cleanup = null;
+      this.state = 'stopped';
+      notify('stopped');
+      return this;
+    });
   }
 };
 
--- modules/express/express-app.test.js
+++ modules/express/express-app.test.js
@@ -1,5 +1,6 @@
 import mongodb from 'mongodb';
 import ExpressApp from './express-app.mjs';
+import Model from '../mongodb/model.mjs';
 
 jest.mock('mongodb');
 const { MongoClient, MongoParseError } = mongodb;
@@ -33,34 +34,36 @@ describe('API server tests', () => {
     expect.assertions(8);
 
     const events = [];
-    const app = new ExpressApp('Test', 3000);
+    const app = new ExpressApp('App', 3000);
 
     app.addObservers(makeObserver(events));
     await app.start(); // Start server
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     await app.start(); // Restart server after being stopped
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server after restart
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     await app.start(); // Retart server again
     expect(app.server.listening).toBe(true);
     await app.start(); // Try to start server while it's already started
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server after second restart
-    expect(app.server).toBeNull();
-    const startEvent = {
-      subjectName: 'Test',
-      componentName: 'server',
-      event: 'listening',
-      data: { port: 3000 },
-    };
-    const stopEvents = [
-      { subjectName: 'Test', componentName: 'server', event: 'stop received', data: undefined },
-      { subjectName: 'Test', componentName: 'server', event: 'closed', data: undefined },
+    expect(app.state).toBe('stopped');
+    const startEvents = [
+      { subjectName: 'App', componentName: 'server', event: 'starting', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App', componentName: 'server', event: 'started', data: undefined },
     ];
-    const startStopEvents = [startEvent, ...stopEvents];
+    const stopEvents = [
+      { subjectName: 'App', componentName: 'server', event: 'stopping', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stop received', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closing', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stopped', data: undefined },
+    ];
+    const startStopEvents = [...startEvents, ...stopEvents];
     expect(events).toStrictEqual([...startStopEvents, ...startStopEvents, ...startStopEvents]);
   });
 
@@ -68,82 +71,52 @@ describe('API server tests', () => {
     expect.assertions(4);
 
     const events = [];
-    const app = new ExpressApp('Test A', 3000);
-    const app2 = new ExpressApp('Test B', 3000);
+    const app = new ExpressApp('App A', 3000);
+    const app2 = new ExpressApp('App B', 3000);
 
     app.addObservers(makeObserver(events));
     app2.addObservers(makeLightObserver(events));
     await app.start(); // Start first server
     expect(app.server.listening).toBe(true);
     await app2.start(); // Try to start second server on the same port
-    expect(app2.server).toBeNull();
+    expect(app2.state).toBe('stopped');
     await app.stop(); // Stop first server
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     expect(events).toStrictEqual([
-      { subjectName: 'Test A', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App A', componentName: 'server', event: 'starting', data: undefined },
+      { subjectName: 'App A', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App A', componentName: 'server', event: 'started', data: undefined },
+      { subjectName: 'App B', componentName: 'server', event: 'starting' },
       {
-        subjectName: 'Test B',
+        subjectName: 'App B',
         componentName: 'server',
         event: 'listen EADDRINUSE: address already in use :::3000',
       },
-      { subjectName: 'Test B', componentName: 'server', event: 'stop received' },
-      { subjectName: 'Test B', componentName: 'server', event: 'not listening' },
-      { subjectName: 'Test A', componentName: 'server', event: 'stop received', data: undefined },
-      { subjectName: 'Test A', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'App B', componentName: 'server', event: 'stopping' },
+      { subjectName: 'App B', componentName: 'server', event: 'stop received' },
+      { subjectName: 'App B', componentName: 'server', event: 'not listening' },
+      { subjectName: 'App B', componentName: 'server', event: 'stopped' },
+      { subjectName: 'App A', componentName: 'server', event: 'stopping', data: undefined },
+      { subjectName: 'App A', componentName: 'server', event: 'stop received', data: undefined },
+      { subjectName: 'App A', componentName: 'server', event: 'closing', data: undefined },
+      { subjectName: 'App A', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'App A', componentName: 'server', event: 'stopped', data: undefined },
     ]);
   });
 
-  test('Start and stop server with a database connection', async () => {
+  test('Start and stop server with a model', async () => {
     expect.assertions(8);
     MongoClient.prototype.isConnected.mockReturnValue(true);
 
     const events = [];
     const app = new ExpressApp(
-      'Test',
-      3000,
-      [{ host: 'db-host', db: 'myDb', username: 'user', password: 'password' }],
-    );
-
-    app.addObservers(makeObserver(events));
-    await app.start(); // Start server
-    expect(app.server.listening).toBe(true);
-    await app.stop(); // Stop server
-    expect(app.server).toBeNull();
-    await app.start(); // Restart server after being stopped
-    expect(app.server.listening).toBe(true);
-    await app.stop(); // Stop server after restart
-    expect(app.server).toBeNull();
-    await app.start(); // Retart server again
-    expect(app.server.listening).toBe(true);
-    await app.start(); // Try to start server while it's already started
-    expect(app.server.listening).toBe(true);
-    await app.stop(); // Stop server after second restart
-    expect(app.server).toBeNull();
-    const startEvents = [
-      { subjectName: 'Test', componentName: 'myDb', event: 'connecting', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb', event: 'connected', data: undefined },
-      { subjectName: 'Test', componentName: 'server', event: 'listening', data: { port: 3000 } },
-    ];
-    const stopEvents = [
-      { subjectName: 'Test', componentName: 'server', event: 'stop received', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb', event: 'disconnected', data: undefined },
-      { subjectName: 'Test', componentName: 'server', event: 'closed', data: undefined },
-    ];
-    const startStopEvents = [...startEvents, ...stopEvents];
-    expect(events).toStrictEqual([...startStopEvents, ...startStopEvents, ...startStopEvents]);
-  });
-
-  test('Start and stop server with multiple database connections', async () => {
-    expect.assertions(8);
-    MongoClient.prototype.isConnected.mockReturnValue(true);
-
-    const events = [];
-    const app = new ExpressApp(
-      'Test',
+      'App',
       3000,
       [
-        { host: 'db-host', db: 'myDb', username: 'user', password: 'password' },
-        { host: 'db-host-2', db: 'myDb2', username: 'user', password: 'password' },
+        new Model(
+          'Model',
+          [{ host: 'db-host', db: 'myDb', username: 'user', password: 'password' }],
+        ),
       ],
     );
 
@@ -151,90 +124,197 @@ describe('API server tests', () => {
     await app.start(); // Start server
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     await app.start(); // Restart server after being stopped
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server after restart
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     await app.start(); // Retart server again
     expect(app.server.listening).toBe(true);
     await app.start(); // Try to start server while it's already started
     expect(app.server.listening).toBe(true);
     await app.stop(); // Stop server after second restart
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     const startEvents = [
-      { subjectName: 'Test', componentName: 'myDb', event: 'connecting', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb2', event: 'connecting', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb', event: 'connected', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb2', event: 'connected', data: undefined },
-      { subjectName: 'Test', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App', componentName: 'server', event: 'starting', data: undefined },
+      { subjectName: 'Model', componentName: null, event: 'connecting', data: undefined },
+      { subjectName: 'Model', componentName: 'myDb', event: 'connecting', data: undefined },
+      { subjectName: 'Model', componentName: 'myDb', event: 'connected', data: undefined },
+      { subjectName: 'Model', componentName: null, event: 'connected', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App', componentName: 'server', event: 'started', data: undefined },
     ];
     const stopEvents = [
-      { subjectName: 'Test', componentName: 'server', event: 'stop received', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb', event: 'disconnected', data: undefined },
-      { subjectName: 'Test', componentName: 'myDb2', event: 'disconnected', data: undefined },
-      { subjectName: 'Test', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stopping', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stop received', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closing', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'Model', componentName: null, event: 'disconnecting', data: undefined },
+      { subjectName: 'Model', componentName: null, event: 'disconnect received', data: undefined },
+      { subjectName: 'Model', componentName: 'myDb', event: 'disconnecting', data: undefined },
+      { subjectName: 'Model', componentName: 'myDb', event: 'disconnected', data: undefined },
+      { subjectName: 'Model', componentName: null, event: 'disconnected', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stopped', data: undefined },
     ];
     const startStopEvents = [...startEvents, ...stopEvents];
     expect(events).toStrictEqual([...startStopEvents, ...startStopEvents, ...startStopEvents]);
   });
 
-  test('Can\'t start the server if the database host is invalid', async () => {
-    expect.assertions(3);
+  test('Start and stop server with multiple models', async () => {
+    expect.assertions(8);
+    MongoClient.prototype.isConnected.mockReturnValue(true);
+
+    const events = [];
+    const app = new ExpressApp(
+      'App',
+      3000,
+      [
+        new Model(
+          'Model A',
+          [{ host: 'db-host', db: 'myDb', username: 'user', password: 'password' }],
+        ),
+        new Model(
+          'Model B',
+          [{ host: 'db-host-2', db: 'myDb2', username: 'user', password: 'password' }],
+        ),
+      ],
+    );
+
+    app.addObservers(makeObserver(events));
+    await app.start(); // Start server
+    expect(app.server.listening).toBe(true);
+    await app.stop(); // Stop server
+    expect(app.state).toBe('stopped');
+    await app.start(); // Restart server after being stopped
+    expect(app.server.listening).toBe(true);
+    await app.stop(); // Stop server after restart
+    expect(app.state).toBe('stopped');
+    await app.start(); // Retart server again
+    expect(app.server.listening).toBe(true);
+    await app.start(); // Try to start server while it's already started
+    expect(app.server.listening).toBe(true);
+    await app.stop(); // Stop server after second restart
+    expect(app.state).toBe('stopped');
+    const startEvents = [
+      { subjectName: 'App', componentName: 'server', event: 'starting', data: undefined },
+      { subjectName: 'Model A', componentName: null, event: 'connecting', data: undefined },
+      { subjectName: 'Model A', componentName: 'myDb', event: 'connecting', data: undefined },
+      { subjectName: 'Model B', componentName: null, event: 'connecting', data: undefined },
+      { subjectName: 'Model B', componentName: 'myDb2', event: 'connecting', data: undefined },
+      { subjectName: 'Model A', componentName: 'myDb', event: 'connected', data: undefined },
+      { subjectName: 'Model B', componentName: 'myDb2', event: 'connected', data: undefined },
+      { subjectName: 'Model A', componentName: null, event: 'connected', data: undefined },
+      { subjectName: 'Model B', componentName: null, event: 'connected', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'listening', data: { port: 3000 } },
+      { subjectName: 'App', componentName: 'server', event: 'started', data: undefined },
+    ];
+    const stopEvents = [
+      { subjectName: 'App', componentName: 'server', event: 'stopping', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stop received', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closing', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'closed', data: undefined },
+      { subjectName: 'Model A', componentName: null, event: 'disconnecting', data: undefined },
+      {
+        subjectName: 'Model A',
+        componentName: null,
+        event: 'disconnect received',
+        data: undefined,
+      },
+      { subjectName: 'Model B', componentName: null, event: 'disconnecting', data: undefined },
+      {
+        subjectName: 'Model B',
+        componentName: null,
+        event: 'disconnect received',
+        data: undefined,
+      },
+      { subjectName: 'Model A', componentName: 'myDb', event: 'disconnecting', data: undefined },
+      { subjectName: 'Model B', componentName: 'myDb2', event: 'disconnecting', data: undefined },
+      { subjectName: 'Model A', componentName: 'myDb', event: 'disconnected', data: undefined },
+      { subjectName: 'Model B', componentName: 'myDb2', event: 'disconnected', data: undefined },
+      { subjectName: 'Model A', componentName: null, event: 'disconnected', data: undefined },
+      { subjectName: 'Model B', componentName: null, event: 'disconnected', data: undefined },
+      { subjectName: 'App', componentName: 'server', event: 'stopped', data: undefined },
+    ];
+    const startStopEvents = [...startEvents, ...stopEvents];
+    expect(events).toStrictEqual([...startStopEvents, ...startStopEvents, ...startStopEvents]);
+  });
+
+  test('Can\'t start the server if the model database host is invalid', async () => {
+    expect.assertions(2);
     const reason = new MongoParseError();
     reason.message = 'URI does not have hostname, domain name and tld';
     MongoClient.prototype.connect.mockRejectedValueOnce(reason);
 
     const events = [];
     const app = new ExpressApp(
-      'Test',
+      'App',
       3000,
-      [{ host: 'notahost', db: 'myDb', username: 'user', password: 'password' }],
+      [
+        new Model(
+          'Model',
+          [{ host: 'notahost', db: 'myDb', username: 'user', password: 'password' }],
+        ),
+      ],
     );
 
     app.addObservers(makeLightObserver(events));
     await app.start(); // Start server
-    expect(app.server).toBeNull();
-    await app.stop(); // Stop server
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     const startEvents = [
-      { subjectName: 'Test', componentName: 'myDb', event: 'connecting' },
+      { subjectName: 'App', componentName: 'server', event: 'starting' },
+      { subjectName: 'Model', componentName: null, event: 'connecting' },
+      { subjectName: 'Model', componentName: 'myDb', event: 'connecting' },
       {
-        subjectName: 'Test',
+        subjectName: 'Model',
         componentName: 'myDb',
         event: 'URI does not have hostname, domain name and tld',
       },
+      { subjectName: 'Model', componentName: null, event: 'disconnecting' },
+      { subjectName: 'Model', componentName: null, event: 'disconnect received' },
+      { subjectName: 'Model', componentName: 'myDb', event: 'not connected' },
+      { subjectName: 'Model', componentName: null, event: 'disconnected' },
     ];
     const stopEvents = [
-      { subjectName: 'Test', componentName: 'server', event: 'stop received' },
-      { subjectName: 'Test', componentName: 'myDb', event: 'not connected' },
+      { subjectName: 'App', componentName: 'server', event: 'stopping' },
+      { subjectName: 'App', componentName: 'server', event: 'stop received' },
+      { subjectName: 'App', componentName: 'server', event: 'stopped' },
     ];
     expect(events).toStrictEqual([...startEvents, ...stopEvents]);
   });
 
   test('Can\'t start the server if the database fails to connect', async () => {
-    expect.assertions(3);
+    expect.assertions(2);
     MongoClient.prototype.isConnected.mockReturnValueOnce(false);
 
     const events = [];
     const app = new ExpressApp(
-      'Test',
+      'App',
       3000,
-      [{ host: 'db-host', db: 'myDb', username: 'user', password: 'password' }],
+      [
+        new Model(
+          'Model',
+          [{ host: 'db-host', db: 'myDb', username: 'user', password: 'password' }],
+        ),
+      ],
     );
 
     app.addObservers(makeLightObserver(events));
     await app.start(); // Start server
-    expect(app.server).toBeNull();
-    await app.stop(); // Stop server
-    expect(app.server).toBeNull();
+    expect(app.state).toBe('stopped');
     const startEvents = [
-      { subjectName: 'Test', componentName: 'myDb', event: 'connecting' },
-      { subjectName: 'Test', componentName: 'myDb', event: 'failed to connect' },
+      { subjectName: 'App', componentName: 'server', event: 'starting' },
+      { subjectName: 'Model', componentName: null, event: 'connecting' },
+      { subjectName: 'Model', componentName: 'myDb', event: 'connecting' },
+      { subjectName: 'Model', componentName: 'myDb', event: 'failed to connect' },
+      { subjectName: 'Model', componentName: null, event: 'disconnecting' },
+      { subjectName: 'Model', componentName: null, event: 'disconnect received' },
+      { subjectName: 'Model', componentName: 'myDb', event: 'not connected' },
+      { subjectName: 'Model', componentName: null, event: 'disconnected' },
     ];
     const stopEvents = [
-      { subjectName: 'Test', componentName: 'server', event: 'stop received' },
-      { subjectName: 'Test', componentName: 'myDb', event: 'not connected' },
+      { subjectName: 'App', componentName: 'server', event: 'stopping' },
+      { subjectName: 'App', componentName: 'server', event: 'stop received' },
+      { subjectName: 'App', componentName: 'server', event: 'stopped' },
     ];
     expect(events).toStrictEqual([...startEvents, ...stopEvents]);
   });
